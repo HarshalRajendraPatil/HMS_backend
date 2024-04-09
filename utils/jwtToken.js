f30323ba6821aec532eb3caad2c3b1a11b7afaaf
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2024-04-09 20:58:02 +0530

    solved a bug

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -7,7 +7,6 @@ export const generateToken = (user, message, statuscode, res) => {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
-      httpOnly: true,
     })
     .json({
       success: true,
